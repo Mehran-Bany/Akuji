c7d883c221eedbf269675b09faebee5c0d1e7c9a
Mehran-Bany <mehrankarim57@gmail.com>
2021-04-19 18:00:30 +0300

    just importing some stuff

--- Akuji.py
+++ Akuji.py
@@ -1,2 +1,11 @@
-# for Akuji project
+# for Akuji Project
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog as fd
+import tkinter.font as tkFont
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
