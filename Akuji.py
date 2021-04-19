245b48f9a3407e7b99f393ac2fe0e407773e84e5
Mehran-Bany <mehrankarim57@gmail.com>
2021-04-19 22:46:15 +0300

    Main canvas size fixed!

--- Akuji.py
+++ Akuji.py
@@ -1,4 +1,4 @@
-# for Akuji Project
+# for Akuji project
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,3 +9,23 @@ import tkinter.font as tkFont
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.figure import Figure
+
+class App:
+    def __init__(self, root):
+        # setting title
+        root.title("Show me what you got")
+        # setting window size
+        width = 1000
+        height = 600
+        screenwidth = root.winfo_screenwidth()
+        screenheight = root.winfo_screenheight()
+        alignstr = '%dx%d+%d+%d' % (width, height,
+                                    (screenwidth - width) / 2, (screenheight - height) / 2)
+        root.geometry(alignstr)
+        root.resizable(width=False, height=False)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
