a73699407b65f28803f0f954013c22fdb210e556
Mehran-Bany <mehrankarim57@gmail.com>
2021-04-21 19:13:18 +0300

    Windows icon changed!

--- Akuji.py
+++ Akuji.py
@@ -6,7 +6,7 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog as fd
 import tkinter.font as tkFont
-
+import time as tm
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 from matplotlib.figure import Figure
 
@@ -23,9 +23,26 @@ class App:
                                     (screenwidth - width) / 2, (screenheight - height) / 2)
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
+    
+
+        '''self.my_window = tk.Tk(root)
+        self.my_window.title('Current Time')
+        ft = tkFont.Font(family='Times', size=10)
+        self.current_time=tm.strftime('%H: %M: %S')
+        self.clock_lable=tk.Label(my_window,font='ariel 80',bg='blue',fg='red',text=current_time)
+        self.clock_lable.grid(row=0,column=0)'''
+
+
+
+
+
 
 
 if __name__ == "__main__":
     root = tk.Tk()
     app = App(root)
+    root.tk.call('wm', 'iconphoto', root._w, tk.PhotoImage(file='python.png'))
     root.mainloop()
+
+
+    #my_window.mainloop()
